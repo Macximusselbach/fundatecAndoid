a208abe18a017d21281b60e415d11c48db3b5b74
Espindula <lucasespindula4@gmail.com>
2022-08-05 22:19:47 -0300

    aula 5

---
+++ splashScreen/app/src/main/java/br/com/selbach/LoginActivity.kt
@@ -0,0 +1,23 @@
+package br.com.selbach
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val loginBtn = findViewById<Button>(R.id.loginButton)
+        loginBtn.setOnClickListener {
+
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+
+        }
+
+    }
+}
